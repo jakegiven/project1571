5a0fb1e5d9521c5ebf206dcc8e6d6860a5483ae4
Ryan Nyborg <rnyborg@email.arizona.edu>
2018-02-17 09:57:12 -0700

    Add xcode project

---
+++ cracking-ciphers/cracking-ciphers/cipher.h
@@ -0,0 +1,17 @@
+//
+//  cipher.h
+//  cracking-ciphers
+//
+//  Created by Ryan Nyborg on 2/17/18.
+//  Copyright © 2018 Ryan Nyborg. All rights reserved.
+//
+
+#ifndef cipher_h
+#define cipher_h
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+#endif /* cipher_h */
---
+++ cracking-ciphers/cracking-ciphers/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  cracking-ciphers
+//
+//  Created by Ryan Nyborg on 2/17/18.
+//  Copyright © 2018 Ryan Nyborg. All rights reserved.
+//
+
+#include "cipher.h"
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // handle .txt file input
+    fopen(argv[1], "r");
+    
+    
+    
+    return 0;
+}
